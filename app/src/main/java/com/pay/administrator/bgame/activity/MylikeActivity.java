376bfffeafdc582d5376d7cc52e43947060e14a7
zhougangwei <892537848@qq.com>
2019-08-27 08:17:20 +0800

    千万23124124141

---
+++ app/src/main/java/com/pay/administrator/bgame/activity/MylikeActivity.java
@@ -0,0 +1,26 @@
+package com.pay.administrator.bgame.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pay.administrator.bgame.R;
+import com.pay.administrator.bgame.base.BaseActivity;
+
+public class MylikeActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_mylike;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
