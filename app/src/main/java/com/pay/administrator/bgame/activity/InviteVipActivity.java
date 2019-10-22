0ed4509edfe5f762560dbff2f0d18dc748ec05ee
wjy <493628242@qq.com>
2019-10-22 19:15:37 +0800

    是电风扇的

--- app/src/main/java/com/pay/administrator/bgame/activity/InviteVipActivity.java
+++ app/src/main/java/com/pay/administrator/bgame/activity/InviteVipActivity.java
@@ -2,6 +2,8 @@ package com.pay.administrator.bgame.activity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pay.administrator.bgame.R;
@@ -11,8 +13,12 @@ import com.pay.administrator.bgame.http.BaseCosumer;
 import com.pay.administrator.bgame.http.RetrofitFactory;
 import com.pay.administrator.bgame.utils.ResultUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -21,21 +27,23 @@ public class InviteVipActivity extends BaseActivity {
 
     @BindView(R.id.tv_invi_code)
     TextView tvInviCode;
-
+    @BindView(R.id.iv1)
+    ImageView iv1;
+    @BindView(R.id.iv2)
+    ImageView iv2;
+    @BindView(R.id.iv3)
+    ImageView iv3;
+    @BindView(R.id.iv4)
+    ImageView iv4;
+    @BindView(R.id.iv5)
+    ImageView iv5;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    List<ImageView> viewList;
     @Override
     protected void initData() {
-
-    }
-
-    @Override
-    protected int getContentViewId() {
-        return R.layout.activity_vip_invite;
-    }
-
-    @Override
-    protected void initView() {
-
-
         RetrofitFactory.getInstance().getuserInfo()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -48,11 +56,36 @@ public class InviteVipActivity extends BaseActivity {
                             int invitedNum = data.getInvitedNum();
                             //数目
 
-
+                            for (ImageView imageView : viewList) {
+                                imageView.setImageResource(R.drawable.tab_icon_me0);
+                            }
+                            for (int num = 0; num<invitedNum; num++) {
+                                viewList.get(num).setImageResource(R.drawable.tab_icon_me1);
+                            }
                         }
                     }
                 });
-
     }
 
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_vip_invite;
+    }
+
+    @Override
+    protected void initView() {
+        tvTitle.setText(R.string.open_vip);
+        viewList=new ArrayList<>();
+        viewList.add(iv1);
+        viewList.add(iv2);
+        viewList.add(iv3);
+        viewList.add(iv4);
+        viewList.add(iv5);
+        initData();
+    }
+
+
+    @OnClick(R.id.iv_back)
+    public void onViewClicked() {
+    }
 }
